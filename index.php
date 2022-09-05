1e3700e59da28461339eb0da0306f01343f7c04b
Tri Putra Satriawan <tripsatriawan30@gmail.com>
2022-09-05 16:57:49 +0700

    add file

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE HTML>
+<!--
+	Solid State by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+    <title>EREN PAFRUM - YOUR THE BEST PARFUM</title>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet"
+          href="assets/css/main.css" />
+    <link rel="stylesheet"
+          href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet"
+          href="styleslide.css">
+    <noscript>
+        <link rel="stylesheet"
+              href="assets/css/noscript.css" />
+    </noscript>
+</head>
+
+<body class="is-preload">
+
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <header id="header"
+                class="alt">
+            <h1><a href="index.html">EREN PAFRUM</a></h1>
+            <nav>
+                <a href="#menu">Menu</a>
+            </nav>
+        </header>
+
+        <!-- Menu -->
+        <nav id="menu">
+            <div class="inner">
+                <h2>Menu</h2>
+                <ul class="links">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="generic.html">Generic</a></li>
+                    <li><a href="elements.html">Elements</a></li>
+                    <li><a href="#">Log In</a></li>
+                    <li><a href="#">Sign Up</a></li>
+                </ul>
+                <a href="#"
+                   class="close">Close</a>
+            </div>
+        </nav>
+
+        <!-- Banner -->
+        <section id="banner">
+            <div class="inner">
+                <div class="logo"><span class="icon fa-gem"></span></div>
+                <h2>PARFUM TERBAIK SAAT INI !</h2>
+                <P>Parfum Pemikat Wanita Dengan Aroma 3 Sekaligus Bikin Wanita Terpikat</P>
+            </div>
+        </section>
+
+        <!-- Wrapper -->
+        <section id="wrapper">
+
+            <!-- One -->
+            <section id="one"
+                     class="wrapper spotlight style1">
+                <div class="inner">
+                    <a href="#"
+                       class="image"><img src="images/parfum1.jpg"
+                             alt="" /></a>
+                    <div class="content">
+                        <h2 class="major">Nama Parfum</h2>
+                        <p>Deskripsi Parfum</p>
+
+                    </div>
+                </div>
+            </section>
+
+            <!-- Two -->
+            <section id="two"
+                     class="wrapper alt spotlight style2">
+                <div class="inner">
+                    <a href="#"
+                       class="image"><img src="images/parfum2.jpg"
+                             alt="" /></a>
+                    <div class="content">
+                        <h2 class="major">Nama Parfum</h2>
+                        <p>Deskripsi Parfum</p>
+
+                    </div>
+                </div>
+            </section>
+
+            <!-- Three -->
+            <section id="three"
+                     class="wrapper spotlight style3">
+                <div class="inner">
+                    <a href="#"
+                       class="image"><img src="images/parfum3.jpg"
+                             alt="" /></a>
+                    <div class="content">
+                        <h2 class="major">Nama Parfum</h2>
+                        <p>Deskripsi Parfum</p>
+
+                    </div>
+                </div>
+            </section>
+
+            <!-- Four -->
+            <section id="four"
+                     class="wrapper alt style1">
+                <div class="inner">
+                    <h2 class="major">PAKE EREN'S PERMASALAHAN DI BAWAH TUNTAS !</h2>
+
+                    <section class="features">
+                        <article>
+                            <div class="row">
+                                <div class="">
+                                    <a href="#"
+                                       class="image"><img class="img-fluid"
+                                             src="images/semprot.jpg"
+                                             width="250px"
+                                             alt="" /></a>
+                                </div>
+                                <div class="">
+                                    <h3 class="major">Mau Parfum berkelas yang wanginya tahan lama?</h3>
+                                </div>
+                            </div>
+                        </article>
+                        <article>
+                            <a href="#"
+                               class="image"><img src="images/nutup.jpg"
+                                     alt="" /></a>
+                            <h3 class="major">Aroma parfum kurang enak & terlalu menyengat? </h3>
+                        </article>
+                        <article>
+                            <a href="#"
+                               class="image"><img src="images/wanitapria.jpg"
+                                     alt="" /></a>
+                            <h3 class="major">pengen parfum yang bikin wanita terpikat?</h3>
+                        </article>
+                    </section>
+                    <h2 class="major">Eren'S Parfum</h2>
+                    <p>Parfum yang aromanya buat kamu tenggelam dalam romansa cinta dan hanyut dalam momen romantis yang
+                        manis dan penuh energi sangat cocok digunakan sehari hari</p>
+                    <ul class="actions">
+                        <li><a href="#"
+                               class="button">PESAN SEKARANG</a></li>
+                    </ul>
+                </div>
+            </section>
+
+        </section>
+
+        <!-- Footer -->
+        <section id="footer">
+            <div class="inner">
+                <h2 class="major">MASIH GA YAKIN? LIAT TESTINYA NIH ! </h2>
+                <div class="container">
+                    <div class="block-heading">
+                        <!-- <h2 class="testimonials">Testimonials</h2> -->
+                        <!-- <div class="row gtr-uniform">
+                            <div class="col-6"><span class="image fit">
+                                    <img src="images/testi1.png"
+                                         alt="" /></span></div>
+                            <div class="col-6"><span class="image fit">
+                                    <img src="images/testi1.png"
+                                         alt="" /></span></div>
+                        </div> -->
+                        <!-- Slideshow container -->
+                        <div class="slideshow-container">
+
+                            <!-- Full-width images with number and caption text -->
+                            <div class="mySlides fade">
+                                <div class="numbertext">1 / 3</div>
+                                <img src="images/testi1.png"
+                                     style="width:100%">
+                                <div class="text">Caption Text</div>
+                            </div>
+
+                            <div class="mySlides fade">
+                                <div class="numbertext">2 / 3</div>
+                                <img src="images/testi1.png"
+                                     style="width:100%">
+                                <div class="text">Caption Two</div>
+                            </div>
+
+                            <div class="mySlides fade">
+                                <div class="numbertext">3 / 3</div>
+                                <img src="images/testi1.png"
+                                     style="width:100%">
+                                <div class="text">Caption Three</div>
+                            </div>
+
+                            <!-- Next and previous buttons -->
+                            <a class="prev"
+                               onclick="plusSlides(-1)">&#10094;</a>
+                            <a class="next"
+                               onclick="plusSlides(1)">&#10095;</a>
+                        </div>
+                        <br>
+
+                        <!-- The dots/circles -->
+                        <div style="text-align:center">
+                            <span class="dot"
+                                  onclick="currentSlide(1)"></span>
+                            <span class="dot"
+                                  onclick="currentSlide(2)"></span>
+                            <span class="dot"
+                                  onclick="currentSlide(3)"></span>
+                        </div>
+                    </div>
+
+                    <h2 class="major">BUKTI PENGIRIMAN BARANG KAMI ! </h2>
+                    <div class="row gtr-uniform">
+                        <div class="col-6"><span class="image fit">
+                                <img src="images/paket1.jpg"
+                                     alt="" /></span></div>
+                        <div class="col-6"><span class="image fit">
+                                <img src="images/paket1.jpg"
+                                     alt="" /></span></div>
+                        <div class="col-6"><span class="image fit">
+                                <img src="images/paket1.jpg"
+                                     alt="" /></span></div>
+                        <div class="col-6"><span class="image fit">
+                                <img src="images/paket1.jpg"
+                                     alt="" /></span></div>
+
+                        <h2 class="major">JANGAN KELAMAAN HARGA SPECIAL HARI INI! </h2>
+                        <div class="col-12"><span class="image fit">
+                                <img src="images/harga.png"
+                                     alt="" /></span></div>
+                    </div>
+
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+
+                    <ul class="actions">
+                        <li><a href="#"
+                               class="button"
+                               style="background-color: #4C5C96;">PESAN SEKARANG</a></li>
+                    </ul>
+
+
+
+
+                    <ul class="copyright">
+                        <li>&copy; Eren Inc. All rights reserved.</li>
+                        <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li>Modifed: <a href="https://www.instagram.com/zoneweb.id/">ZONEWEB.ID</a></li>
+                    </ul>
+                </div>
+        </section>
+
+    </div>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="styleslide.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
